5449affdc12955d51cb628667c38ee39e91f1626
Hugo Goncalves <hugo.goncalves@joyn.de>
2025-05-29 11:02:01 +0200

    fix: variable names

--- packages/landing-page/src/components/landing/ContactForm.tsx
+++ packages/landing-page/src/components/landing/ContactForm.tsx
@@ -59,7 +59,7 @@ export default function Formulario() {
 
   const onSubmit = async (data: FormUserData) => {
     try {
-      const res = await fetch(`${process.env.API_URL}user`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}user`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -88,7 +88,7 @@ export default function Formulario() {
     const postalCode = e.target.value;
     try {
       const res = await fetch(
-        `${process.env.TOMTOM_API_URL}${postalCode}.json?typeahead=false&limit=1&countrySet=pt&extendedPostalCodesFor=addr&minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&relatedPois=off&key=${process.env.TOMTOM_API_KEY}`
+        `${process.env.NEXT_PUBLIC_TOMTOM_API_URL}${postalCode}.json?typeahead=false&limit=1&countrySet=pt&extendedPostalCodesFor=addr&minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&relatedPois=off&key=${process.env.NEXT_PUBLIC_TOMTOM_API_KEY}`
       );
       if (!res.ok) throw new Error('Erro ao buscar endereço');
       const { results } = await res.json();
