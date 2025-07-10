91b3eab9579f776ba59a0e2e726bda22caa9a2b9
Hugo Goncalves <hugo.goncalves@joyn.de>
2025-07-10 21:30:21 +0200

    chore(portal): init

---
+++ packages/portal/src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    <div className="font-family-poppins">
+      <div></div>
+      <footer className="text-xs text-center text-white  bg-gradient-horizontal py-6">
+        <p>
+          Política de Privacidade | Política de Cookies | Livro de Reclamações
+          Online
+        </p>
+      </footer>
+    </div>
+  );
+}
